09fe0a143320abd121a604252268d2c60e203d01
Amanda Dolci <amanda.dolci@gmail.com>
2024-05-29 18:05:36 -0300

    feat: improve product list structure

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
+// import { ProductCard } from '../ProductCard';
+// import { useState } from 'react';
+import { StyledProductList, StyledProductCard } from './style';
 
-export function ProductList({ productList }) {
+export function ProductList({ productList, favoriteProduct }) {
+	// const applyClass = index =>
+	// 	index === productList.length - 1
+	// 		? 'product-container-card last-row-item'
+	// 		: 'product-container-card';
+	const applyFavoriteClass = favorite =>
+		favorite ? 'material-symbols-rounded material-symbols-filled' : 'material-symbols-rounded';
+
 	return (
-		<ul>
-			{productList.map(({ id, name, image_url, price, favorite }) => (
-				<li key={id}>
-					<figure>
+		<StyledProductList className='product-container-list'>
+			{productList.map(({ id, name, image_url, price, favorite }, index) => (
+				<StyledProductCard key={id} /* className={applyClass(index)} */>
+					<figure className='product-card-img'>
+						<button
+							onClick={() => (favorite ? favoriteProduct(id, false) : favoriteProduct(id, true))}>
+							<span className={applyFavoriteClass(favorite)}>favorite</span>
+						</button>
 						<img src={`http://localhost:3000${image_url}`} alt={name} />
 					</figure>
-					<div>
+					<div className='product-card-info'>
 						<h2>{name}</h2>
 						<h3>{price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</h3>
-						<button onClick={() => handleClick(id)}>Ver mais</button>
-						<button onClick={() => handleClick(id)}>
-							<span className='material-symbols-rounded'>favorite</span>
-						</button>
 					</div>
-				</li>
+				</StyledProductCard>
 			))}
-		</ul>
+		</StyledProductList>
 	);
 }
+
+// <ProductCard
+// 	key={id}
+// 	id={id}
+// 	name={name}
+// 	image_url={image_url}
+// 	price={price}
+// 	favorite={favorite}
+// />
